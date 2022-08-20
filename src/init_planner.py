60fe446460e1e1bb6902e1d228f1fbf75e0fdcbf
Arnav Thareja <athareja@cs.washington.edu>
2022-08-20 15:52:24 -0700

    Add new testcases and randomness

--- src/init_planner.py
+++ src/init_planner.py
@@ -7,6 +7,16 @@ import time
 import math
 from mushr_coordination.msg import GoalPoseArray
 from std_msgs.msg import String
+import random
+from copy import deepcopy
+
+use_mocap_start = False  # set to true to use the mocap position as the start position, false to use the testcase start position
+
+INVALID_POSE_Z = -100.0
+mocap_poses = []
+
+def mocap_callback(p, i):
+    mocap_poses[i] = p
 
 def angle_to_quaternion(angle):
     """Convert an angle in radians into a quaternion _message_."""
@@ -20,12 +30,23 @@ if __name__ == "__main__":
     num_agent = rospy.get_param("init_clcbs/num_agent")
     num_task = rospy.get_param("init_clcbs/num_task")
     num_waypoint = rospy.get_param("init_clcbs/num_waypoint")
+    randomness = rospy.get_param("init_clcbs/randomness")
+    x_min = rospy.get_param("init_clcbs/minx")
+    x_max = rospy.get_param("init_clcbs/maxx")
+    y_min = rospy.get_param("init_clcbs/miny")
+    y_max = rospy.get_param("init_clcbs/maxy")
     pubs = []
+    mocap_subs = []
     # this is basically initializing all the subscribers for counting the number of cars and publishers for initiailizing pose and goal points.
     for i in range(num_agent):
         name = rospy.get_param("init_clcbs/car" + str(i+1) + "/name")
         publisher = rospy.Publisher(name + "/init_pose", PoseStamped, queue_size=5)
         pubs.append(publisher)
+        if use_mocap_start:
+            mocap_poses.append(PoseStamped())
+            mocap_poses[i].pose.position.z = INVALID_POSE_Z
+            mocap_sub = rospy.Subscriber("/" + name + "/car_pose", PoseStamped, callback=mocap_callback, callback_args=i)
+            mocap_subs.append(mocap_sub)
     goal_pub = rospy.Publisher("/mushr_coordination/goals", GoalPoseArray, queue_size=5)
     obs_pub = rospy.Publisher("/mushr_coordination/obstacles", PoseArray, queue_size=5)
     clcbs_obs_pub = rospy.Publisher("/clcbs_ros/obstacles", PoseArray, queue_size=5)
@@ -36,10 +57,14 @@ if __name__ == "__main__":
         carmsg = PoseStamped()
         carmsg.header.frame_id = "/map"
         carmsg.header.stamp = now
-        carmsg.pose.position.x = rospy.get_param("init_clcbs/car" + str(i + 1) + "/x")
-        carmsg.pose.position.y = rospy.get_param("init_clcbs/car" + str(i + 1) + "/y")
-        carmsg.pose.position.z = 0.0
-        carmsg.pose.orientation = angle_to_quaternion(rospy.get_param("init_clcbs/car" + str(i + 1) + "/t"))
+        if use_mocap_start and mocap_poses[i].pose.position.z != INVALID_POSE_Z:  # make sure mocap has published for this agent
+            carmsg.pose = deepcopy(mocap_poses[i].pose)
+            carmsg.pose.position.z = 0.0
+        else:
+            carmsg.pose.position.x = min(x_max, max(x_min, rospy.get_param("init_clcbs/car" + str(i + 1) + "/x") + random.uniform(-randomness[0], randomness[0])))
+            carmsg.pose.position.y = min(y_max, max(y_min, rospy.get_param("init_clcbs/car" + str(i + 1) + "/y") + random.uniform(-randomness[1], randomness[1])))
+            carmsg.pose.position.z = 0.0
+            carmsg.pose.orientation = angle_to_quaternion(rospy.get_param("init_clcbs/car" + str(i + 1) + "/t"))
         print(carmsg)
         rospy.sleep(1)
         pubs[i].publish(carmsg)
@@ -54,10 +79,10 @@ if __name__ == "__main__":
     goalmsg.header.frame_id = "/map"
     goalmsg.header.stamp = now
     goalmsg.scale = rospy.get_param("init_clcbs/scale")
-    goalmsg.minx = rospy.get_param("init_clcbs/minx")
-    goalmsg.miny = rospy.get_param("init_clcbs/miny")
-    goalmsg.maxx = rospy.get_param("init_clcbs/maxx")
-    goalmsg.maxy = rospy.get_param("init_clcbs/maxy")
+    goalmsg.minx = x_min
+    goalmsg.miny = y_min
+    goalmsg.maxx = x_max
+    goalmsg.maxy = y_max
     for i in range(num_task):
         goalmsg.goals.append(PoseArray())
         for j in range(num_waypoint):
